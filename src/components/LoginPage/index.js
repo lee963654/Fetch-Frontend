b371621caf772e68935593f3eee80d5447300bf8
James Lee <lee963654@gmail.com>
2023-12-11 22:10:06 -0500

    working on homepage styling

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,18 +1,18 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { loginThunk } from "../../store/session";
+import "./LoginPage.css"
 
 
 export default function LoginPage() {
     const history = useHistory()
-    const dispatch = useDispatch()
     const [name, setName] = useState("")
     const [email, setEmail] = useState("");
+    const [error, setError] = useState("")
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const user = {name: name, email: email}
+        const user = { name: name, email: email }
         const requestOptions = {
             method: "POST",
             headers: {
@@ -23,37 +23,50 @@ export default function LoginPage() {
         }
         const response = await fetch("https://frontend-take-home-service.fetch.com/auth/login", requestOptions)
         if (response.ok) {
-            console.log("response login", response)
-            dispatch(loginThunk(user))
             history.push("/")
+        } else {
+            setError("Please enter a valid email address")
         }
     }
 
+    useEffect(() => {
+        setError("")
+    }, [email])
+
 
     return (
-        <>
-            <h1>Log In</h1>
+        <div className="login-page-container">
+            <section>
+                <p>
+                    Welcome to Fetch! We love dogs, and hope you do too! We are here to help you bring a lucky dog home. Please enter your name and email to get started.
+                </p>
+            </section>
             <form onSubmit={handleSubmit}>
-                <label>
-                    Name
-                    <input
-                        type="text"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>
-                    Email
-                    <input
-                        type="text"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </label>
+                <div className="form-labels">
+                    <label>
+                        <span>Name</span>
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            required
+                            placeholder="Enter Your Name"
+                        />
+                    </label>
+                    <label>
+                        <span>Email</span>
+                        <input
+                            type="text"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                            placeholder="Enter Your Email"
+                        />
+                    </label>
+                    {error && <span className="errors">{error}</span>}
+                </div>
                 <button type="submit">Log In</button>
             </form>
-        </>
+        </div>
     );
 }
