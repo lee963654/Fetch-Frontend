8ff5cb2ae48e4308fa70f32d92815ead4ebe20bf
James Lee <lee963654@gmail.com>
2023-12-13 01:17:02 -0500

    working on more filters

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -4,6 +4,20 @@ import "./Pagination.css"
 
 export default function Pagination({ resultsPerPage, totalResults, handleSearch }) {
     const [page, setPage] = useState(0)
+    const [currentPageStart, setCurrentPageStart] = useState(0)
+    const [currentPageEnd, setCurrentPageEnd] = useState(11)
+
+
+    // const pageBlockStart = page % 10 === 0 ? page : 0
+    // const pageBlockEnd = pageBlockStart === page ? page + 10 : 0
+    // const pageBlockStart = () => {
+    //     if (page % 10 === 0) {
+    //         currPageStart = page
+    //     }
+    //     console.log("THE CURR PAGE START", currPageStart)
+
+    // }
+    // console.log("TESTING PAGEBLOCKSTART", pageBlockStart())
 
     //Testing
     // const [pageNumbers, setPageNumbers] = useState([])
@@ -17,8 +31,6 @@ export default function Pagination({ resultsPerPage, totalResults, handleSearch 
     const nextPage = () => {
         if (page === pageNumbers.length - 1) return pageNumbers.length - 1
         const nextPageCount = page + 1
-        console.log("THIS IS IN THE NEXT PAGE FUNTION FOR PAGE", page)
-        console.log("THIS IS IN THE NEXT PAGE FUNTION FOR NEXTPAGECOUNT", nextPageCount)
         setPage(nextPageCount)
         return nextPageCount
     }
@@ -32,14 +44,47 @@ export default function Pagination({ resultsPerPage, totalResults, handleSearch 
     console.log("THIS IS THE CURENT PAGE IN THE PAGINATION", page)
 
     //testing
-    // useEffect(() => {
-    //     const pageNumArray = []
-    //     for (let i = 1; i <= Math.ceil(totalResults / resultsPerPage); i++) {
-    //         pageNumArray.push(i)
-    //     }
-    //     setPageNumbers(pageNumArray)
-    // }, [])
+    useEffect(() => {
+        console.log("USEEFFECT TEST")
+        const pageBlockStart = () => {
+            console.log("TESTING THE PAGE BLOCK START FGUNCTION")
+            const lastPage = pageNumbers[pageNumbers.length - 1]
+            if (page < 10 || page === 0) {
+                setCurrentPageStart(0)
+                setCurrentPageEnd(11)
+                return
+            }
+            if (page % 10 === 0 && page > currentPageStart) {
+                setCurrentPageStart(page - 1)
+                setCurrentPageEnd(page + 11)
+                return
+            }
+            // if (page % 10 === 0 && page <= currentPageStart) {
+            //     setCurrentPageStart(page - 1)
+            //     setCurrentPageEnd(page + 11)
+            //     return
+            // }
+            if (page === currentPageStart) {
+                setCurrentPageStart(page - 10)
+                setCurrentPageEnd(end => end - 10)
+                return
+            }
+            if (page + 1 === lastPage) {
+                const getFirstDigit = String(page).charAt(0)
+                const stringFirstDigit = `${getFirstDigit - 1}9`
+                const currStart = Number(stringFirstDigit)
+                console.log("THE CURR START IN THE LAST STEP", currStart)
+                setCurrentPageStart(currStart)
+                setCurrentPageEnd(currStart + 12)
+                console.log("IN THE LAST PAGE SETUP")
+            }
 
+            console.log("CHECKING THE LAST PAGE IN THE USEEFFECT", lastPage)
+        }
+        pageBlockStart()
+    }, [page])
+    console.log("THE CURR PAGE START AND END", currentPageStart, currentPageEnd)
+    console.log("WE ARE SLICING THE PAGE NUMBERS BLOCK", pageNumbers.slice(currentPageStart, currentPageEnd))
 
     return (
 
